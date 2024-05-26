ad77f0d891dbcb39adb1bbf0cdd1c035577ac565
ThiagoGalvan <galvanabbondanzathiago@gmail.com>
2024-05-25 21:04:27 -0300

    saque system para evitar incompatibilidades con otros sistemas. puse getchar doble

--- Trabajos/Codigos/Equivalencia cm-pies&pulgadas/Equivalencia cm-pies&pulgadas.c
+++ Trabajos/Codigos/Equivalencia cm-pies&pulgadas/Equivalencia cm-pies&pulgadas.c
@@ -11,6 +11,7 @@ int main(){
     pulgadas=cm/2.54;
     pies=cm/30.48;
     printf("La equivalencia en pies es de %d y en pulgadas es de %f\n",pies,pulgadas);
-    system("pause");
+    getchar();
+    getchar();
     return 0;
 }
