62bd522fe817070c313f6861ff3c9196d9c109fd
ThiagoGalvan <galvanabbondanzathiago@gmail.com>
2024-05-31 15:55:35 -0300

    arregle el codigo porque hacia la conversion de pies y pulgadas por separado. No sabia que la conversión se hacia de los pies y pulgadas juntos.

--- Trabajos/Codigos/11.Equivalencia cm-pies-pulgadas/Equivalencia-cm-pies-pulgadas.c
+++ Trabajos/Codigos/11.Equivalencia cm-pies-pulgadas/Equivalencia-cm-pies-pulgadas.c
@@ -1,15 +1,16 @@
-//Desarrolla un algoritmo que permita, dados ciertos centímetros como entrada de tipo 
+//11.Desarrolla un algoritmo que permita, dados ciertos centímetros como entrada de tipo 
 //flotante, emitir por pantalla su equivalencia en pies (enteros) y en pulgadas.
 
 #include <stdio.h>
 
 int main(){
-    float cm,pulgadas;
+    float cm,pulgadas,pulgadas_resto;
     int pies;
     printf("Introduce la cantidad de centimetros:\n");
     scanf("%f",&cm);
     pulgadas=cm/2.54;
     pies=cm/30.48;
-    printf("La equivalencia en pies es de %d y en pulgadas es de %f\n",pies,pulgadas);
+    pulgadas_resto=pulgadas-pies*12;
+    printf("La equivalencia en pies es de %d y en pulgadas es de %f\n",pies,pulgadas_resto);
     return 0;
 }
