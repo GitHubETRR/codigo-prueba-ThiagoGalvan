f6e5e51a10a1dc507f717161b589aa63f98653ee
ThiagoGalvan <galvanabbondanzathiago@gmail.com>
2024-06-07 14:03:13 -0300

    arregle tabulaciones

--- Trabajos/Codigos/21.dia_mes_año_DDMMAAAA/dia_mes_año_DDMMAAAA.c
+++ Trabajos/Codigos/21.dia_mes_año_DDMMAAAA/dia_mes_año_DDMMAAAA.c
@@ -4,35 +4,40 @@
 #include <stdio.h>
 
 int main(){
-    int DDMMAAAA,days,month,year,bisiesto;
+    int DDMMAAAA,days,month,year,bisiesto,redo;
     printf("En este programa al ingresar una fecha en el formato DDMMAAAA se va a poder separar en Dia, mes y Anio\n");
-    printf("Ingrese la fecha a convertir(DDMMAAAA):\n");
-    scanf("%d",&DDMMAAAA);
-    
-    year=DDMMAAAA%10000;
-    month=(DDMMAAAA/10000)%100;
-    days=DDMMAAAA/1000000;
-    
-    if(days>30 && (4==month||6==month||9==month||11==month)){
-        printf("El mes que ingreso no tiene mas 31 dias");
-    }
-    else if (2==month && 29<days)
-    {
-        bisiesto=year%4;
-        if(bisiesto==0)
-        {
-            printf("Año:%d Mes:%d Dia:%d",year,month,days);
-        }else
-        {
-            printf("El anio no es bisiesto por lo que febrero no tiene 29 dias");
+    do{
+        printf("Ingrese la fecha a convertir(DDMMAAAA):\n");
+        scanf("%d",&DDMMAAAA);
+        year=DDMMAAAA%10000;
+        month=(DDMMAAAA/10000)%100;
+        days=DDMMAAAA/1000000;
+        if(month>12){
+            printf("Error, el mes debe ser menor o igual a 12\n");
         }
-    }else if(days>31 && (1==month || 3==month || 5==month || 7==month || 8==month ||10==month || 12==month)){
-        printf("El mes que ingreso no tiene mas de 31 dias ");}
-        else{
-            printf("Anio:%d Mes:%d Dia:%d",year,month,days);
+        else if(days>30 && (4==month||6==month||9==month||11==month)){
+            printf("El mes que ingreso no tiene mas 31 dias");
         }
+        else if (2==month && 29<days){
+            bisiesto=year%4;
+            if(bisiesto==0)
+            {
+                printf("Año:%d Mes:%d Dia:%d",year,month,days);
+            }else
+            {
+                printf("El anio no es bisiesto por lo que febrero no tiene 29 dias");
+            }
+        }else if(days>31 && (1==month || 3==month || 5==month || 7==month || 8==month ||10==month || 12==month)){
+            printf("El mes que ingreso no tiene mas de 31 dias ");}
+            else{
+                printf("Anio:%d Mes:%d Dia:%d",year,month,days);
+            }
+            printf("¿Quiere ingresar otra fecha?\n");
+            printf("(0)No\n");
+            printf("(1)Si\n");
+            scanf("%d",&redo);
+    }while(redo==1);
     getchar();
     getchar();
     return 0;
     }
-
